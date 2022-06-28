9640996e83435b4db54b7238ceeef4a73b0cc3da
Dennis Nguyen <dnsnguy08@gmail.com>
2022-06-27 18:54:44 -0700

    finished building logic for readme questions

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const axios = require('axios');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [
+    {
+        message: "What is your project title?",
+        name: "title",
+        type: "input",
+    },
+    {
+        message: "Please provide your project's description",
+        name: "description",
+        type: "input",
+    },
+    {
+        message: "Please provide the installation instructions.",
+        name: "installation",
+        type: "input",
+    },
+    {
+        message: "Please provide the project usage.",
+        name: "usage",
+        type: "input",
+    },
+    {
+        message: "Please select a license for your application.",
+        name: "license",
+        type: "list",
+        choices: [`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+        `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`,
+        `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`]
+    },
+    {
+        message: "Please provide the contributing instructions.",
+        name: "contributing",
+        type: "input",
+    },
+    {
+        message: "Please provide any test instructions.",
+        name: "test",
+        type: "input",
+    },
+    {
+        message: "What is your github user name?",
+        name: "username",
+        type: "input",
+    },
+    {
+        message: "What is your repo link?",
+        name: "repo",
+        type: "input",
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const accountURL = `https://api.github.com/users/${data.username}`;
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+        axios.get(accountURL)
+        .then(account => {
+            const accountInfo = {
+                email: account.data.email,
+                profile: account.data.html_url,
+                name: account.data.name
+            };
+            fs.writeFile("README.md", generateMarkdown(data, accountInfo), function(err) {
+                if (err) {
+                    throw err;
+            };
+            console.log("New README Success!");
+          });
+        });
+});
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,52 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// variable for license link based off of user selection
+let licenseLink = "";
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`){
+    licenseLink = `MIT License: https://choosealicense.com/licenses/mit/`;
+  } else if (license === `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`){
+    licenseLink = `GNU License: https://choosealicense.com/licenses/gpl-2.0/`;
+  } else if (license === `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`){
+    licenseLink = `BSD License: https://choosealicense.com/licenses/bsd-3-clause/`;
+  }
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+// Function to generate markdown for README
+function generateMarkdown(data, accountInfo) {
+  renderLicenseSection(data.license);
   return `# ${data.title}
-
+  ${data.license}
+  ## Description 
+  ${data.description}
+  ## Table of contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Licence](#licence)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [Repository Link](#repository)
+  - [Questions](#questions) 
+  ## Installation
+          ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${licenseLink}
+  ## Contributing
+  ${data.contributing}
+  ## Test
+  ${data.test}
+  ## Repository
+  - [Project Repo](${data.repo})
+  ## Questions
+  For any questions, please reach out to:
+  - ${accountInfo.name}
+  - [GitHub Profile](${accountInfo.profile})
+  - <${accountInfo.email}>  
 `;
 }
 
